1247d3acdd8dfbcc150104ce3c940bf8669ca632
Cathy Lill <cathygail.lill@gmail.com>
2016-03-22 14:57:29 +1100

    draftjs plugin in progress

--- plugins/wiki/components/wikiWidget.js
+++ plugins/wiki/components/wikiWidget.js
@@ -1,22 +1,37 @@
 import styles from './style.css';
+//import stylesDraft from '../../../node_modules/draft-js/dist/Draft.css';
 
 import React, { PropTypes } from 'react';
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, RichUtils} from 'draft-js';
 import update from 'react-addons-update';
 
 import { searchInText } from '..';
 import { log } from '../../../core';
 import createWidgetContainer from '../../../core/containers/widgetContainer';
 
+function updateWiki () {
+  log(arguments);
+}
 
 function wikiWidget(props) {
   const { state } = props;
   const onChange = (editorState) => update(state, { editorState: { $set: editorState }});
-
+  const handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(state.editorState, command);
+    if (newState) {
+      onChange(newState);
+      return true;
+    }
+    return false;
+  }
   state.editorState = EditorState.createEmpty();
 
   return (
-    <Editor editorState={state.editorState} onChange={onChange} />
+    <Editor
+      editorState={state.editorState}
+      handleKeyCommand={handleKeyCommand}
+      onChange={updateWiki}
+      placeholder="Enter some text..." />
   );
 }
 
